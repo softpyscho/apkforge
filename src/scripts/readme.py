9507dd6755efd6ffd38414390718026996829671
unknown <chandurisantosh@gmail.com>
2026-08-02 15:44:38 +0530

    updations

--- src/scripts/readme.py
+++ src/scripts/readme.py
@@ -224,8 +224,8 @@ def _obtainium_link(entry) -> str:
     
     encoded_payload = urllib.parse.quote(json.dumps(payload), safe="")
     deep_link = f"https://apps.obtainium.imranr.dev/redirect?r=obtainium://app/{encoded_payload}"
-    badge_img = '![Add to Obtainium](https://raw.githubusercontent.com/ImranR98/Obtainium/main/assets/graphics/badge_obtainium.png)'
-    return f"[{badge_img}]({deep_link})"
+    badge_img = '<img src="https://raw.githubusercontent.com/ImranR98/Obtainium/main/assets/graphics/badge_obtainium.png" alt="Add to Obtainium" height="35">'
+    return f"<a href=\"{deep_link}\">{badge_img}</a>"
 
 
 def generate_apps_section() -> str:
